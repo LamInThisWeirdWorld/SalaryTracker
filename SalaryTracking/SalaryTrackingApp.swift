6267cd143842c0bc389ff15d601e00336e5c4c03
LamInThisWeirdWorld <lamishere1715@gmail.com>
2025-07-11 16:11:20 +1000

    Initial Commit

---
+++ SalaryTracking/SalaryTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalaryTrackingApp.swift
+//  SalaryTracking
+//
+//  Created by Nguyen Ngoc Thanh Lam on 11/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct SalaryTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SalaryTrackingTests/SalaryTrackingTests.swift
@@ -0,0 +1,17 @@
+//
+//  SalaryTrackingTests.swift
+//  SalaryTrackingTests
+//
+//  Created by Nguyen Ngoc Thanh Lam on 11/7/2025.
+//
+
+import Testing
+@testable import SalaryTracking
+
+struct SalaryTrackingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
